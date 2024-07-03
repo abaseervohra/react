213e5e7b92d33f3d3a8e6fe24fa77768f6e8bd2c
Your Name <youremail@example.com>
2024-07-03 17:06:02 +0500

    update react website

---
+++ src/Card/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Card.module.css";
+
+const Card = (props) => {
+  console.log(props);
+  return (
+    <div className={styles.card}>
+      <img
+        className={props.i}
+        src="https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg"
+        alt=""
+      />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </div>
+  );
+};
+
+export default Card;
